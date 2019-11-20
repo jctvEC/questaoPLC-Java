f3c68a05d175c1eb8e9492bf3a0791b1f27a3739
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2019-11-20 13:13:14 -0300

    Corr1g1ndo var1avel

--- questao.java
+++ questao.java
@@ -6,7 +6,7 @@ public class Questao {
 
   public static void main(String [ ] args) { 
 
-    integer a,b,c,d,e ; 
+    Integer a,b,c,d,e ; 
     int f ; 
     char r1,r2,r3,r4,r5 ;   
     String s ; 
